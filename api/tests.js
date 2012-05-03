f9e04e7384a1d775a2205e05d54d80f2e156438d
Daniel Pérez Rada <dperezrada@gmail.com>
2012-05-02 23:16:53 -0400

    Refactor

--- api/tests.js
+++ api/tests.js
@@ -1,33 +1,32 @@
-var request = require('superagent');
-var assert = require('assert');
+var request = require('superagent'),
+	assert = require('assert'),
+	_ = require('underscore');
 
-describe('/accounts are the corps that have bought the product', function(){
-  describe('Create an account', function(){
-    it('should create an account successfully ', function(done){
-      	request
-			.post('http://localhost:3000/accounts')
-			.set('Content-Type', 'application/json')
-			.send({
+describe('Accounts', function(){
+	describe('POST /accounts with minimal needed parameters', function(){
+		before(function(){
+			this.account_data = {
 		        'email': 'daniel@payparrot.com',
 		        'name': 'Daniel',
 		        'startup': 'Payparrot',
 		        'url': 'http://payparrot.com/',
-			})
- 			.end(function(res1){
-				assert.equal(201,res1.statusCode);
-				var id = res1.body.id;
-				request.get('http://localhost:3000/accounts/'+id).end(function(res2){
-					assert.equal(200,res2.statusCode);
-					assert.deepEqual({
-						'id': id,
-				        'email': 'daniel@payparrot.com',
-				        'name': 'Daniel',
-				        'startup': 'Payparrot',
-				        'url': 'http://payparrot.com/',
-					},res2.body);
-					done();
-				});
- 			});
+			}
+		});
+    	it('should create a new account', function(done){
+			var self = this;
+      		request
+				.post('http://localhost:3000/accounts')
+				.set('Content-Type', 'application/json')
+				.send(self.account_data)
+	 			.end(function(res1){
+					assert.equal(201,res1.statusCode);
+					var id = res1.body.id;
+					request.get('http://localhost:3000/accounts/'+id).end(function(res2){
+						assert.equal(200,res2.statusCode);
+						assert.deepEqual(_.extend(self.account_data, {'id': id}),res2.body);
+						done();
+					});
+	 			});
 		})
 	})
 })
