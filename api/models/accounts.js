1975048c57daf00011f60e5606e1280132e38a21
Daniel Pérez Rada <dperezrada@gmail.com>
2012-05-05 13:15:20 -0400

    Refactor returnJSON function

--- api/models/accounts.js
+++ api/models/accounts.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var _ = require('underscore');
 
 var accounts_schema = new Schema({
  	email: String,
- 	password: String,
+ 	password: {type: String, private: true},
 	name: String,	     
 	startup: String,
 	url: String,
 	id: String,
-	credentials: []
+	credentials: {type: Schema.Types.Mixed, private: true}
 });
 // TODO: Check problem with redefine
 accounts_schema.pre('save', function(next){
@@ -21,9 +22,12 @@ module.exports = Accounts = mongoose.model('Accounts');
 
 Accounts.prototype.returnJSON = function(){
 	var to_return = this.toJSON();
+	_.each(accounts_schema.tree, function(value, key){
+		if(value.private){
+			delete to_return[key];
+		}
+	});
 	to_return['id'] = to_return['_id'];
-	delete to_return['password'];
-	delete to_return['credentials'];
 	delete to_return['_id'];
 	return to_return;
 }
