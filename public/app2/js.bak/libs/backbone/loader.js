464d51145844aa198b4017eec5a8cd3445738688
Daniel Pérez Rada <dperezrada@gmail.com>
2012-05-17 11:23:33 -0400

    Removing Trash

--- public/app2/js.bak/libs/backbone/loader.js
+++
@@ -1,8 +0,0 @@
-define(['libs/require/order!libs/jquery/jquery.orig', 'libs/require/order!libs/underscore/underscore-min', 'libs/require/order!libs/backbone/backbone-min','libs/require/order!libs/jquery/tojson'],
-function(){
-  return {
-    Backbone: Backbone.noConflict(),
-    _: _.noConflict(),
-    $: jQuery.noConflict()
-  };
-});
--- public/app2/js.bak/libs/jquery/jquery-wrapper.js
+++
@@ -1,5 +0,0 @@
-define(["libs/jquery/jquery.orig"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    console.log($);
-    return $;
-});
