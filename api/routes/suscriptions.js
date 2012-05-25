e7ea006cd9a1360cf662b7369f727ca492ce41c2
Daniel Guajardo <daniel.guajardo.k@gmail.com>
2012-05-25 17:46:49 -0400

    Notifications controller

--- api/routes/suscriptions.js
+++ api/routes/suscriptions.js
@@ -1,7 +1,13 @@
  Suscriptions = require('payparrot_models/objects/suscriptions.js'),
 
  exports.validate = function(req, res) {
- 	Suscriptions.findOne({_id: req.params.id_suscription},function(err,suscription){
+ 	var params = req.query
+ 	_id = 1
+ 	account_id = 2
+	params['_id'] = req.params.id_suscription;
+	params['account_id'] = req.params.account_id;
+
+ 	Suscriptions.findOne(params,function(err,suscription){
  		if (suscription) {
  			res.statusCode = 200;
  			res.send(suscription);
