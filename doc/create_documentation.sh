8e2dcce47ec1d0990690aa0539f4e689f80c1792
Daniel Pérez Rada <dperezrada@gmail.com>
2012-06-07 10:10:02 -0400

    Adding updated documentation.

--- doc/create_documentation.sh
+++ doc/create_documentation.sh
@@ -2,4 +2,4 @@ DIR=`dirname $0`
 cd $DIR
 DIR=`pwd`
 
-ant xhtml -Doutputdir=$PWD -Dapidir=$PWD/base -Dapiname=payparrot -f $PWD/libs/rest-doc-template/build.xml
+ant all -Doutputdir=$PWD -Dapidir=$PWD/base -Dapiname=payparrot -f $PWD/libs/rest-doc-template/build.xml
