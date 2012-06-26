b27190b4681c93f561b47f5d2fea01985c83b297
Daniel Pérez Rada <dperezrada@gmail.com>
2012-06-26 12:40:57 -0400

    New oauth tokens

--- models/libs/twitter_oauth.js
+++ models/libs/twitter_oauth.js
@@ -6,8 +6,8 @@ var twitter_config = {
 	'authorize_url': 'https://api.twitter.com/oauth/authorize',
 	'access_token_url': 'https://api.twitter.com/oauth/access_token',
 	'callback_url': config.twitter_callback_url,
-	'consumer_key': 'lFkPrTmvjcSUD5JtrOvg',
-	'consumer_secret': 'sCxLuVAd1HnGIjdolKUqAjZaSOO7BGhViD1a7w',
+	'consumer_key': 'ygPaFatlRYsx1LqaJvFINA',
+	'consumer_secret': '6v8AVGvbsd0t5M8CyDrhBLKks8PFoAfBeuxd508',
 	'version': '1.0',
 	'signature': 'HMAC-SHA1'
 };
