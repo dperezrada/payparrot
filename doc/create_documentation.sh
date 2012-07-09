58f5d346c7129bc81f90b885a78abaf71ab49605
Daniel Guajardo <daniel.guajardo.k@gmail.com>
2012-07-09 16:05:54 -0400

    Preparing to move from nodejs to python

--- doc/create_documentation.sh
+++
@@ -1,5 +0,0 @@
-DIR=`dirname $0`
-cd $DIR
-DIR=`pwd`
-
-ant all -Doutputdir=$PWD -Dapidir=$PWD/base -Dapiname=payparrot -f $PWD/libs/rest-doc-template/build.xml
--- public/js/libs/jquery/tojson_loader.js
+++
@@ -1,9 +0,0 @@
-define([ "libs/require/order!libs/jquery/jquery"
-       , "libs/require/order!libs/jquery/tojson"
-       ]
-
-       , function($) {
-           // Raw sammy does not return anything, so return it explicitly here.
-           return $.fn.toJSON;
-         }
-      );
