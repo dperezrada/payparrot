259d90a61ae6d62d4c99db23e944089acfbc7b50
Daniel Guajardo <daniel.guajardo.k@gmail.com>
2012-07-19 12:28:52 -0400

    Handling twitter exceptions

--- dal/payparrot_dal/twitter.py
+++ dal/payparrot_dal/twitter.py
@@ -3,6 +3,7 @@ import oauth2 as oauth
 from urllib import urlencode
 import os
 
+
 class Twitter():
 	consumer_key = os.environ.get("PAYPARROT_TWITTER_KEY")
 	consumer_secret = os.environ.get("PAYPARROT_TWITTER_SECRET")
@@ -10,12 +11,13 @@ class Twitter():
 	access_token_url = 'https://api.twitter.com/oauth/access_token'
 	authorize_url = 'https://api.twitter.com/oauth/authorize'
 	oauth_callback = os.environ.get("PAYPARROT_TWITTER_CALLBACK")
+	timeout = 15
 
 	def __init__(self):
 		self.consumer = oauth.Consumer(self.consumer_key, self.consumer_secret)
 
 	def create_session(self):
-		client = oauth.Client(self.consumer)
+		client = oauth.Client(self.consumer, timeout = self.timeout)
 		return client
 
 	def get_request_token(self,client):
@@ -31,7 +33,7 @@ class Twitter():
 	def get_access_tokens(self,oauth_verifier, request_token):
 		token = oauth.Token(request_token['oauth_token'],request_token['oauth_token_secret'])
 		token.set_verifier(oauth_verifier)
-		client = oauth.Client(self.consumer, token)
+		client = oauth.Client(self.consumer, token, timeout = self.timeout)
 
 		resp, content = client.request(self.access_token_url, "POST")
 		access_token = dict(urlparse.parse_qsl(content))
@@ -39,7 +41,7 @@ class Twitter():
 
 	def create_client(self,oauth_token, oauth_token_secret):
 		token = oauth.Token(oauth_token, oauth_token_secret)
-		self.client = oauth.Client(self.consumer, token)
+		self.client = oauth.Client(self.consumer, token, timeout = self.timeout)
 		return True
 
 	def get(self, url):
